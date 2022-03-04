92348aaeb9d4694f7f0788ea3ec0b266870a125a
Aram Hekimian <aramhek121@gmail.com>
2022-03-03 19:09:16 -0600

    Added JS functionality to buttons

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 // Create functions for basic math operators
+let firstNum;
+let secondNum;
+let answer;
 function add(a, b) {
-    let sum = a + b;
-    console.log(sum);
-    return sum;
+    answer = a + b;
+    return answer;
 }
 
 function subtract(a, b) {
-    let sub = a - b;
-    console.log(sub);
-    return sub;
+    answer = a - b;
+    return answer;
 }
 
 function multiply(a, b) {
-    let product = a * b;
-    console.log(product);
-    return product;
+    answer = a * b;
+    return answer;
 }
 
 function divide(a, b) {
-    let division = a / b;
-    console.log(division);
-    return division;
+    answer = a / b;
+    return answer;
 }
 
 // Create a function that takes an operator and numbers 
@@ -38,11 +37,107 @@ function operate(operator, a, b) {
         divide(a, b);
     }
     else {
-        alert(`${operator}: is not a valid operator.`);
+        alert("ERROR: Please select a digit and an operator.");
     }
 }
 
-operate("divide", 200, 2);
+//Add button click effect
+let allButtons = document.querySelectorAll("button");
+for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("mousedown", function () {
+        allButtons[i].style.backgroundColor = "#404040"
+    })
+}
+for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("mouseup", function () {
+        allButtons[i].style.backgroundColor = "#707070"
+    })
+}
 
+
+// Make the display value functions  and make the display change
+let buttons = document.getElementsByClassName("digits");
+let displayValue = "";
+let screen = document.getElementById("display");
+buttons[0].addEventListener("click", function () {
+    displayValue = displayValue + "1";
+    screen.textContent = displayValue;
+})
+buttons[1].addEventListener("click", function () {
+    displayValue = displayValue + "2";
+    screen.textContent = displayValue;
+})
+buttons[2].addEventListener("click", function () {
+    displayValue = displayValue + "3";
+    screen.textContent = displayValue;
+})
+buttons[3].addEventListener("click", function () {
+    displayValue = displayValue + "4";
+    screen.textContent = displayValue;
+})
+buttons[4].addEventListener("click", function () {
+    displayValue = displayValue + "5";
+    screen.textContent = displayValue;
+})
+buttons[5].addEventListener("click", function () {
+    displayValue = displayValue + "6";
+    screen.textContent = displayValue;
+})
+buttons[6].addEventListener("click", function () {
+    displayValue = displayValue + "7";
+    screen.textContent = displayValue;
+})
+buttons[7].addEventListener("click", function () {
+    displayValue = displayValue + "8";
+    screen.textContent = displayValue;
+})
+buttons[8].addEventListener("click", function () {
+    displayValue = displayValue + "9";
+    screen.textContent = displayValue;
+})
+buttons[9].addEventListener("click", function () {
+    displayValue = displayValue + "0";
+    screen.textContent = displayValue;
+})
+
+// Add Clear button function
+let clear = document.getElementById("clear");
+clear.addEventListener("click", function () {
+    displayValue = "";
+    screen.textContent = displayValue;
+})
+
+// Add operator button funcionality and save first num chosen
+let operators = document.getElementsByClassName("operators");
+let operatorChosen = "";
+operators[0].addEventListener("click", function () {
+    operatorChosen = "add";
+    firstNum = displayValue;
+    displayValue = "";
+})
+operators[1].addEventListener("click", function () {
+    operatorChosen = "subtract";
+    firstNum = displayValue;
+    displayValue = "";
+})
+operators[2].addEventListener("click", function () {
+    operatorChosen = "multiply";
+    firstNum = displayValue;
+    displayValue = "";
+})
+operators[3].addEventListener("click", function () {
+    operatorChosen = "divide";
+    firstNum = displayValue;
+    displayValue = "";
+})
+
+// Make equals button work (Save second num chosen and run operation)
+let equals = document.getElementById("equals");
+equals.addEventListener("click", function () {
+    secondNum = displayValue;
+    operate(operatorChosen, parseInt(firstNum), parseInt(secondNum));
+    screen.textContent = answer;
+    displayValue = "";
+})
 
 
